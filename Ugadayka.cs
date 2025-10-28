02cce552f9d9b6bdd115edaebb73fdd0addaedb3
nighttramp <144950139+NightTrampYT@users.noreply.github.com>
2025-10-28 17:21:05 +0300

    угадайка

---
+++ Ugadayka.cs
@@ -0,0 +1,19 @@
+﻿Random random = new Random();
+Console.Write("Guess the number: ");
+int secretNum = random.Next(1, 101);
+
+int userNum = int.Parse(Console.ReadLine());
+while (userNum != secretNum)
+{
+    if (userNum < secretNum)
+    {
+        Console.WriteLine("You entered number less than the secret one\nEnter new one");
+        userNum = int.Parse(Console.ReadLine());
+    }
+    else if (userNum > secretNum)
+    {
+        Console.WriteLine("You entered number bigger than the secret one\nEnter new one");
+        userNum = int.Parse(Console.ReadLine());
+    }
+}
+Console.WriteLine("You're right!");
